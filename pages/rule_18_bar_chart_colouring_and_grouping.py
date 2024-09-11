f265a629395c9c144d79100106550932ef4a8f25
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2024-09-11 07:11:46 +0200

    Added GDP charts

--- pages/rule_18_bar_chart_colouring_and_grouping.py
+++ pages/rule_18_bar_chart_colouring_and_grouping.py
@@ -9,6 +9,8 @@ from utils.load_data import (
     population_in_extreme_poverty_data
 )
 
+from utils.bar_chart_examples.gdp_per_capita_plot import gdp_per_capita_bar_chart_plot
+
 # ---------------------------------------------------------------------
 # CONFIGURATION
 # ---------------------------------------------------------------------
@@ -22,6 +24,7 @@ pages_format()
 # Data read
 # ---------------------------------------------------------------------
 gdp_per_capita_df = gdp_per_capita_data()
+
 co2_emissions_per_capita_df = co2_emissions_per_capita_data()
 child_mortality_df = child_mortality_data()
 air_pollution_df = air_pollution_data()
@@ -38,18 +41,47 @@ colouring_tab, grouping_tab, subplots_tab  = st.tabs(
      ]
 )
 
+# highlighting a small bar
+
 with colouring_tab:
     st.subheader('xxxx')
     st.write('xxx')
 
     st.markdown("🌐 [Original article used for inspiration](https://www.addtwodigital.com/add-two-blog/2021/7/12/rule-18-dont-use-multi-coloured-bars)")
+    st.markdown("🔗 [To see the code which generated these plots, navigate to the repo](https://github.com/JoseParrenoGarcia/Plotly-great-examples/blob/b75ca0485b4bb1cb71c8d4d7d4e41b1b36dd6cb5/utils/bar_chart_examples/travel_gdp_share_plot.py#L3)")
 
-    st.write(gdp_per_capita_df)
-    st.write(co2_emissions_per_capita_df)
-    st.write(child_mortality_df)
-    st.write(air_pollution_df)
-    st.write(gov_health_expenditure_df)
-    st.write(population_in_extreme_poverty_df)
+    col1, col2 = st.columns(2)
+
+    with col1:
+        with st.container(border=True):
+            st.write('**Highlighting a specific bar**')
+            st.divider()
+            st.plotly_chart(gdp_per_capita_bar_chart_plot(df=gdp_per_capita_df))
+
+    with col2:
+        with st.container(border=True):
+            st.write('**Highlighting the minimum or the maximum**')
+            st.divider()
+            st.plotly_chart(gdp_per_capita_bar_chart_plot(df=gdp_per_capita_df))
+
+    with col1:
+        with st.container(border=True):
+            st.write('**Highlighting a data quality issue**')
+            st.divider()
+            st.plotly_chart(gdp_per_capita_bar_chart_plot(df=gdp_per_capita_df))
+
+    with col2:
+        with st.container(border=True):
+            st.write('**Highlighting a benchmark**')
+            st.divider()
+            st.plotly_chart(gdp_per_capita_bar_chart_plot(df=gdp_per_capita_df))
+
+    # st.write(gdp_per_capita_df)
+    # st.write(co2_emissions_per_capita_df)
+    # st.write(child_mortality_df)
+    # st.write(air_pollution_df)
+    # st.write(gov_health_expenditure_df)
+    # st.write(population_in_extreme_poverty_df)
 
 
     st.write('**Bar chart**')
---
+++ utils/bar_chart_examples/gdp_per_capita_plot.py
@@ -0,0 +1,79 @@
+import plotly.graph_objects as go
+
+def gdp_per_capita_bar_chart_plot(df, highlight='Uruguay'):
+    df = df.sort_values('GDP per capita', ascending=True)
+
+    if highlight=='Uruguay':
+        marker_color_ = ['rgba(85, 181, 229, 1)' if c_ == 'Uruguay' else 'lightgrey' for c_ in df['Entity']]
+        text_ = [f"<b>${val / 1000:,.1f}k</b>" if c_ == 'Uruguay' else '' for val, c_ in zip(df['GDP per capita'], df['Entity'])]
+        title_text = 'Uruguay flies high'
+        subtitle_text = "Uruguay's GDP per capita is amongst the highest in South America (2020)"
+
+    fig = go.Figure(
+        data=[
+            go.Bar(
+                y=df['Entity'],
+                x=df['GDP per capita'],
+                marker_color=marker_color_,
+                orientation='h',
+                text=text_,
+                textposition='outside',
+            )
+        ]
+    )
+
+    # Update the layout
+    fig.update_layout(
+        title=dict(
+            text=title_text,
+            y=0.98,
+            x=0,
+            xanchor='left',
+            yanchor='top',
+            font=dict(family="Helvetica Neue", size=18),
+        ),
+        annotations=[
+            # Second paragraph annotation
+            dict(
+                text=subtitle_text,
+                xref="paper", yref="paper",
+                x=0, y=1.08,
+                showarrow=False,
+                font=dict(family="Helvetica Neue", size=14),
+                align="left"
+            ),
+            # Footer annotation
+            dict(
+                text="Source: World Bank, <a href='https://ourworldindata.org'>Our World in Data</a>",
+                xref="paper", yref="paper",
+                x=0, y=-0.15,
+                showarrow=False,
+                font=dict(family="Helvetica Neue", size=12),
+                align="left"
+            ),
+        ],
+        xaxis=dict(
+            title='',
+            showline=True,
+            linecolor='lightgrey',
+            linewidth=1,
+        ),
+        yaxis=dict(
+            title='',
+            showline=True,
+            linecolor='lightgrey',
+            linewidth=1,
+            showgrid=False,
+        ),
+        font=dict(family="Helvetica Neue"),
+        barmode='group',
+        margin=dict(t=100, pad=0),
+        height=500,
+        width=600,
+    )
+
+    return fig
+
+
+
+
