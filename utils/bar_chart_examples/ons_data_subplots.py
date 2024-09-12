0612b292651fd5832c65cf85d1b268350b25535d
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2024-09-12 22:05:27 +0200

    subplots finished

--- utils/bar_chart_examples/ons_data_subplots.py
+++ utils/bar_chart_examples/ons_data_subplots.py
@@ -15,52 +15,56 @@ def ons_data_subplots_bar_charts(ons_data_df):
     columns_to_fill = ['GDP_per_capita', 'CO2_emissions_per_capita', 'Child_mortality', 'Air_pollution_NOx', 'Gov_health_expenditure', 'Population_in_poverty']
     ons_data_df[columns_to_fill] = ons_data_df[columns_to_fill].fillna(0)
 
-    fig = make_subplots(rows=3, cols=2,
-                        subplot_titles=(
-                            'GDP per Capita',
-                            'CO2 Emissions per Capita',
-                            'Child Mortality',
-                            'Air Pollution (Nitrogen oxide (NOx))',
-                            'Gov Health Expenditure',
-                            'Population in Extreme Poverty')
-                        )
+    fig = make_subplots(
+        rows=3, cols=2,
+        subplot_titles=[
+            'GDP per capita',
+            'Gov. health expenditure (as % of total)',
+            'Child mortality rate',
+            'Population in extreme poverty (<$2.15 p/day)',
+            'CO2 Emissions (per capita)',
+            'Air pollution (NOx)',
+        ], vertical_spacing=0.1)
 
     features = [
         'GDP_per_capita',
-        'CO2_emissions_per_capita',
-        'Child_mortality',
-        'Air_pollution_NOx',
         'Gov_health_expenditure',
+        'Child_mortality',
         'Population_in_poverty',
-    ]
-
-    titles = [
-        'GDP per Capita', 'CO2 Emissions per Capita', 'Child Mortality',
-        'Air Pollution', 'Gov Health Expenditure', 'Population in Extreme Poverty'
-    ]
-
-    subtitles = [
-        "GDP per capita in USD", "CO2 emissions per capita in metric tons",
-        "Child mortality rate per 1000 live births", "Air pollution in PM2.5",
-        "Government health expenditure as % of GDP", "Population in extreme poverty (%)"
+        'CO2_emissions_per_capita',
+        'Air_pollution_NOx',
     ]
 
     for i, feature in enumerate(features):
         row = i // 2 + 1
         col = i % 2 + 1
 
+        print(f'{feature}: {row}x{col}')
+
         # Sort the DataFrame by the current feature
-        sorted_df = ons_data_df.sort_values(by=feature, ascending=True)
+        zero_value_entities = ons_data_df[ons_data_df[feature] == 0]['Entity'].tolist()
+        print(zero_value_entities)
+        print('')
+        sorted_df = ons_data_df[ons_data_df[feature] > 0].sort_values(by=feature, ascending=True).copy()
 
         # Define bar colors
         marker_color_ = ['rgba(85, 181, 229, 1)' if c_ == 'Uruguay' else 'lightgrey' for c_ in sorted_df['Entity']]
 
         if feature == 'GDP_per_capita':
             text_ = [f"<b>${val / 1000:,.1f}k</b>" if c_ == 'Uruguay' else '' for val, c_ in zip(sorted_df[feature], sorted_df['Entity'])]
-        elif feature == 'Child_mortality':
+        elif feature == 'Air_pollution_NOx':
+            text_ = [f"<b>{val / 1000:,.1f}k</b>" if c_ == 'Uruguay' else '' for val, c_ in zip(sorted_df[feature], sorted_df['Entity'])]
+        elif feature in ['Child_mortality', 'CO2_emissions_per_capita']:
             text_ = [f"<b>{val:.2f}</b>" if c_ == 'Uruguay' else '' for val, c_ in zip(sorted_df[feature], sorted_df['Entity'])]
+        elif feature in ['Gov_health_expenditure', 'Population_in_poverty']:
+            text_ = [f"<b>{val:.2f}%</b>" if c_ == 'Uruguay' else '' for val, c_ in zip(sorted_df[feature], sorted_df['Entity'])]
         else:
             text_ = ['' for _ in sorted_df['Entity']]
+
+        if feature == 'Gov_health_expenditure':
+            range_ = [0, 26]
+        else:
+            range_ = [None, None]
 
         # Add bar chart to the subplot
         fig.add_trace(
@@ -76,13 +80,18 @@ def ons_data_subplots_bar_charts(ons_data_df):
             row=row, col=col
         )
 
+        # Update the font, size, and alignment of the subplot titles
+        for annotation in fig['layout']['annotations']:
+            annotation['font'] = dict(family="Helvetica Neue", size=14)
+
         # Update the layout for each subplot
         fig.update_xaxes(
-            title_text=subtitles[i],
+            title_text='',
             showline=True,
             linecolor='lightgrey',
             linewidth=1,
-            row=row, col=col
+            row=row, col=col,
+            range=range_,
         )
         fig.update_yaxes(
             title_text='',
@@ -95,10 +104,15 @@ def ons_data_subplots_bar_charts(ons_data_df):
 
     # Update the overall layout
     fig.update_layout(
-        height=1200, width=1200,
-        title_text="ONS Data Subplots",
+        title=dict(
+            text="Uruguay is one of the most succesful countries in South America",
+            font=dict(family="Helvetica Neue", size=24)  # Change font family and size here
+        ),
         font=dict(family="Helvetica Neue"),
+        barmode='group',
         margin=dict(t=100, pad=0),
+        height=1000,
+        width=800,
     )
 
     return fig
