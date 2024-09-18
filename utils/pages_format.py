235c6ef65a2164c8d85cba3333c90d59e500a509
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2024-09-18 11:29:51 +0200

    Rule 25. Broken bars and non zero starts
    Added page

--- utils/pages_format.py
+++ utils/pages_format.py
@@ -8,6 +8,7 @@ def pages_format():
             Page("pages/rule_17_alternative_to_bar_charts.py", "R17. Bar chart alternatives"),
             Page("pages/rule_18_bar_chart_colouring_and_grouping.py", "R18. Colouring and grouping"),
             Page("pages/rule_19_ordering_bar_charts.py", "R19. Order of bars"),
-            Page("pages/rule_24_labelling_axis.py", "R24. Axis labelling"),
+            Page("pages/rule_24_labelling_axis_bar_charts.py", "R24. Axis labelling"),
+            Page("pages/rule_25_broken_bars_and_non_zero_starts_bar_charts.py", "R25. Non zero starts and broken bars"),
         ]
     )
