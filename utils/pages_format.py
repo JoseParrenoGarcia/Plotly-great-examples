4e0476612d98df96e3e9acfbc3c3d8c9a96a681d
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2024-11-01 06:40:41 +0100

    lines_for_data blog

--- utils/pages_format.py
+++ utils/pages_format.py
@@ -11,5 +11,6 @@ def pages_format():
             Page("pages/rule_24_labelling_axis_bar_charts.py", "R24. Axis labelling"),
             Page("pages/rule_25_broken_bars_and_non_zero_starts_bar_charts.py", "R25. Non zero starts and broken bars"),
             Page("pages/rule_28_clustered_columns.py", "R28. Careful with clustered columns"),
+            Section(name="Rules for line charts", icon="📈"),
         ]
     )
