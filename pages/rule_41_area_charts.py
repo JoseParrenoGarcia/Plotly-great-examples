69561c4ff4e06256c34dd13d7591f40274aaf0cf
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2024-12-09 09:25:40 +0100

    refugees data

--- pages/rule_41_area_charts.py
+++ pages/rule_41_area_charts.py
@@ -3,7 +3,7 @@ from utils.pages_format import pages_format
 
 from utils.load_data import (
     sector_growth_data,
-    market_stocks_data,
+    refugees_data,
 )
 
 # ---------------------------------------------------------------------
@@ -19,7 +19,7 @@ pages_format()
 # Data read
 # ---------------------------------------------------------------------
 sector_growth_data_df = sector_growth_data()
-# market_stocks_data_df = market_stocks_data()
+refugees_data_df = refugees_data()
 # ireland_population_df = ireland_population_data()
 # contraceptive_use_data_df = contraceptive_use_data()
 
@@ -62,8 +62,8 @@ with walkthrough_tab:
     st.write('')
     st.markdown("🌐 [Original article used for inspiration](https://www.addtwodigital.com/add-two-blog/2024/2/23/rule-41-avoid-area-charts")
 
-    # with st.expander("Expand to see the data"):
-    #     st.dataframe(human_height_data_df, hide_index=True)
+    with st.expander("Expand to see the data"):
+        st.dataframe(refugees_data_df, hide_index=True)
 
 with stacked_100_tab:
     st.subheader('100% stacked timeseries')
