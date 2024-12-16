d70e2b24b528d359411c8966e06b4a1d80fd1263
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2024-12-16 15:27:10 +0100

    Adding improvements to timeline plot

--- utils/line_chart_examples/space_race_plot.py
+++ utils/line_chart_examples/space_race_plot.py
@@ -5,43 +5,69 @@ def space_race_line_chart(df):
 
     # Add annotations for the Soviet Union
     soviet_data = df[df['Country'] == 'Soviet Union']
-    for index, row in soviet_data.iterrows():
-        fig.add_annotation(
-            x=-20,  # Position to the left of the y-axis
-            y=row['Year'],
-            text=row['Event'],
-            showarrow=True,
-            arrowhead=2,
-            ax=-100,  # Length of the arrow
-            ay=0,
-        )
+    # for index, row in soviet_data.iterrows():
+    #     fig.add_annotation(
+    #         x=-200,
+    #         y=row['Year'],
+    #         text=row['text_to_show'],
+    #         align='right',
+    #         showarrow=False,
+    #         arrowhead=0,
+    #         ax=0,
+    #         ay=0,
+    #     )
 
-    us_data = df[df['Country'] == 'United States']
-    for index, row in us_data.iterrows():
-        fig.add_annotation(
-            x=20,  # Position to the left of the y-axis
-            y=row['Year'],
-            text=row['Event'],
-            showarrow=True,
-            arrowhead=2,
-            ax=100,  # Length of the arrow
-            ay=0,
+    fig.add_trace(
+        go.Scatter(
+            x=[-35] * len(soviet_data),
+            y=soviet_data['Year'],
+            text=soviet_data['text_to_show'],
+            mode='text',
+            showlegend=False,
+            textposition='middle left'
         )
+    )
 
-    both_data = df[df['Country'] == 'U.S. & USSR']
-    for index, row in both_data.iterrows():
-        fig.add_annotation(
-            x=0,  # Position to the left of the y-axis
-            y=row['Year'],
-            text=row['Event'],
-            showarrow=True,
-            arrowhead=2,
-            ax=0,  # Length of the arrow
-            ay=0,
-        )
+    # us_data = df[df['Country'] == 'United States']
+    # for index, row in us_data.iterrows():
+    #     fig.add_annotation(
+    #         x=20,
+    #         y=row['Year'],
+    #         text=row['text_to_show'],
+    #         align='left',
+    #         showarrow=True,
+    #         arrowhead=0,
+    #         ax=100,
+    #         ay=0,
+    #     )
+    #
+    # both_data = df[df['Country'] == 'U.S. & USSR']
+    # for index, row in both_data.iterrows():
+    #     fig.add_annotation(
+    #         x=0,
+    #         y=row['Year'] + 0.2,
+    #         text=row['Event'],
+    #         showarrow=True,
+    #         arrowhead=0,
+    #         ax=0,
+    #         ay=-50,
+    #     )
 
     fig.add_vline(x=20, line=dict(color="black", width=2))
     fig.add_vline(x=-20, line=dict(color="black", width=2))
+
+    # Add scatter plot for years
+    years = list(range(1957, 1976))
+    fig.add_trace(
+        go.Scatter(
+            x=[0] * len(years),
+            y=years,
+            mode='text',
+            text=[str(year) for year in years],
+            showlegend=False,
+            textposition='middle center'
+        )
+    )
 
     # Update layout
     fig.update_layout(
@@ -49,15 +75,12 @@ def space_race_line_chart(df):
         xaxis=dict(
             showticklabels=False,
             showgrid=False,
-            range=[-300, 300],
+            range=[-600, 600],
             position=0,
             zeroline=False,
-            # zerolinewidth=2,
-            # zerolinecolor='black'
         ),
         yaxis=dict(
-            range=[1957, 1975],
-            # dtick=1,
+            range=[1956, 1976],
             title='',
             showgrid=False,
             visible=False,
@@ -65,7 +88,7 @@ def space_race_line_chart(df):
         showlegend=False,
         margin=dict(t=150, pad=0),
         height=1000,
-        width=700,
+        width=1000,
     )
 
     return fig
