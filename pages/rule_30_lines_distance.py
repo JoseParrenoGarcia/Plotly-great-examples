c66cae440fa3a54399c73b714ba208d668d987ac
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2024-12-16 06:22:50 +0100

    Rule 30 Space race plot

--- pages/rule_30_lines_distance.py
+++ pages/rule_30_lines_distance.py
@@ -57,7 +57,6 @@ with timeseries_tab:
 
     st.write('')
     st.markdown("🌐 [Original article used for inspiration](https://www.addtwodigital.com/add-two-blog/2022/1/28/rule-30-a-line-chart-should-only-show-change-over-time)")
-    # st.markdown("🔗 [To see the code which generated these plots, navigate to the repo](https://github.com/JoseParrenoGarcia/Plotly-great-examples/blob/main/utils/bar_chart_examples/workforce_by_sector_plot.py)")
 
     with st.expander("Expand to see the data"):
         st.dataframe(driving_women_data_df, hide_index=True)
---
+++ utils/line_chart_examples/space_race_plot.py
@@ -0,0 +1,4 @@
+import plotly.graph_objects as go
+
+def space_race_line_chart(df):
+    return df
