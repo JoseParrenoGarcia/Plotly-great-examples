83f6f80d75c817aabe746c439e7e5177b79f9974
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2024-12-19 11:59:50 +0100

    Finished fertility plots

--- pages/rule_31_too_many_lines.py
+++ pages/rule_31_too_many_lines.py
@@ -5,7 +5,7 @@ from utils.load_data import (
     fertility_rates_data,
 )
 
-from utils.line_chart_examples.fertility_rates_plots import fertility_rates_plotly_express_line_chart, fertility_rates_line_plot
+from utils.line_chart_examples.fertility_rates_plots import fertility_rates_plotly_express_line_chart, fertility_rates_line_plot, fertility_rates_lines_by_group
 
 # ---------------------------------------------------------------------
 # CONFIGURATION
@@ -96,12 +96,15 @@ with grey_out_tab:
         )
 
 
-# with other_distances_tab:
-#     st.subheader('Other distances')
-#
-#     st.write('')
-#     st.markdown("🌐 [Original article used for inspiration](https://www.addtwodigital.com/add-two-blog/2022/1/28/rule-30-a-line-chart-should-only-show-change-over-time)")
-#
-#     with st.expander("Expand to see the data"):
-#         st.dataframe(political_view_survey_data_df, hide_index=True)
+with segment_tab:
+    st.subheader('Segment to provide both context and details')
+
+    st.write('')
+    st.markdown("🌐 [Original article used for inspiration](https://www.addtwodigital.com/add-two-blog/2022/5/4/rule-31-line-charts-shouldnt-have-too-many-lines)")
+
+    with st.expander("Expand to see the data"):
+        st.dataframe(fertility_rates_data_df, hide_index=True)
+
+    with st.container(border=True):
+        st.plotly_chart(fertility_rates_lines_by_group(df=fertility_rates_data_df,))
 
--- utils/line_chart_examples/fertility_rates_plots.py
+++ utils/line_chart_examples/fertility_rates_plots.py
@@ -1,5 +1,6 @@
 import plotly.graph_objects as go
 import plotly.express as px
+from plotly.subplots import make_subplots
 
 def fertility_rates_plotly_express_line_chart(df):
     fig = px.line(df,
@@ -116,12 +117,130 @@ def fertility_rates_line_plot(df, title, countries_to_highlight):
     return fig
 
 def fertility_rates_lines_by_group(df):
-    # subplots 5 (asia, africa, europe, north american, south america)
-    # create auxiliary plot
-    # -> take each continent (region), plot all lines in grey, highlight the max min and average
-    # then plot the line plot with
-    # line_chart = create_line_chart(df, title, countries_to_highlight)
-    # for trace in line_chart.data:
-    #     fig.add_trace(trace, row=row, col=col)
+    def _individual_plot(df, continent):
+        df_continent = df[df['continent'] == continent]
+        last_year = df_continent['Year'].max()
+        df_last_year = df_continent[df_continent['Year'] == last_year]
+        max_fertility_country = df_last_year.loc[df_last_year['Fertility_Rate'].idxmax()]
+        min_fertility_country = df_last_year.loc[df_last_year['Fertility_Rate'].idxmin()]
 
-    return df
+        countries_to_highlight = [max_fertility_country['Country Name'], min_fertility_country['Country Name']]
+        rest_of_countries = [country for country in df_continent['Country Name'].unique() if country not in countries_to_highlight]
+
+        fig = go.Figure()
+
+        for country in rest_of_countries:
+            fig.add_trace(
+                go.Scatter(x=df_continent[df_continent['Country Name'] == country]['Year'],
+                           y=df_continent[df_continent['Country Name'] == country]['Fertility_Rate'],
+                           mode='lines',
+                           name=country,
+                           showlegend=False,
+                           line=dict(color='rgba(204, 209, 209, 0.5)', width=1)
+                           )
+                )
+
+        for country in countries_to_highlight:
+            if country in df['Country Name'].unique():
+                fig.add_trace(
+                    go.Scatter(x=df_continent[df_continent['Country Name'] == country]['Year'],
+                               y=df_continent[df_continent['Country Name'] == country]['Fertility_Rate'],
+                               mode='lines',
+                               name=country,
+                               showlegend=False,
+                               line=dict(color='purple', width=2)
+                               )
+                )
+
+                fig.add_trace(
+                    go.Scatter(x=df_continent[(df_continent['Country Name'] == country) & (df_continent['Year'] == df_continent['Year'].max())]['Year'],
+                               y=df_continent[(df_continent['Country Name'] == country) & (df_continent['Year'] == df_continent['Year'].max())]['Fertility_Rate'],
+                               mode='markers+text',
+                               name=country,
+                               showlegend=False,
+                               marker=dict(color='purple', size=6),
+                               text=country,
+                               textposition='bottom left',
+                               textfont=dict(family="Helvetica Neue", color='purple', size=12),
+                               )
+                )
+
+        return fig
+
+    fig = make_subplots(rows=1, cols=5, shared_yaxes=True)
+
+    asia_fig = _individual_plot(df=df, continent='Asia')
+    for trace in asia_fig.data:
+        fig.add_trace(trace, row=1, col=1)
+
+    europe_fig = _individual_plot(df=df, continent='Europe')
+    for trace in europe_fig.data:
+        fig.add_trace(trace, row=1, col=2)
+
+    africa_fig = _individual_plot(df=df, continent='Africa')
+    for trace in africa_fig.data:
+        fig.add_trace(trace, row=1, col=3)
+
+    north_america_fig = _individual_plot(df=df, continent='North America')
+    for trace in north_america_fig.data:
+        fig.add_trace(trace, row=1, col=4)
+
+    south_america_fig = _individual_plot(df=df, continent='South America')
+    for trace in south_america_fig.data:
+        fig.add_trace(trace, row=1, col=5)
+
+    # Update layout
+    fig.update_layout(
+        title=dict(
+            text='Fertility rates are down across the world, but to different degrees.',
+            font=dict(family="Helvetica Neue", size=22),
+        ),
+        annotations=[
+            dict(
+                text="Children per woman",
+                xref="paper",
+                yref="paper",
+                x=-0.04, y=1.06,
+                showarrow=False,
+                font=dict(family="Helvetica Neue", size=14),
+                align="left"
+            ),
+            dict(
+                text="<i>Highlighting the countries with the highest and lowest fertility rate by continent</i>",
+                xref="paper",
+                yref="paper",
+                x=-0.04, y=1.13,
+                showarrow=False,
+                font=dict(family="Helvetica Neue", size=14, color='purple'),
+                align="left"
+            ),
+        ],
+        font=dict(family="Helvetica Neue", size=14),
+        showlegend=False,
+        margin=dict(t=150, r=100, pad=0),
+        height=700,
+    )
+
+    for col in range(1, 6):
+        fig.update_xaxes(
+            showline=True,
+            linecolor='lightgrey',
+            linewidth=2,
+            range=[df['Year'].min(), df['Year'].max() + 2],
+            dtick=10,
+            row=1,
+            col=col
+        )
+
+        fig.update_yaxes(
+            showline=True,
+            showgrid=True,
+            linecolor='lightgrey',
+            linewidth=2,
+            row=1,
+            col=col
+        )
+
+    fig.update_yaxes(title='', ticksuffix="  ", row=1, col=1)
+
+    return fig
